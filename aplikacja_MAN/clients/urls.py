6d948cb91f11ef70ad8b6cd8df5b13d42478fadc
h5326 <jacek.tomczak-ext@man.eu>
2019-06-05 08:34:00 +0200

    skopiowanie modulu clients z PanKanapka

---
+++ aplikacja_MAN/clients/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'clients'
+
+urlpatterns = [
+    # path('', views.clients, name='clients'),
+    path('add', views.add_new_user, name='add_new_user'),
+    path('logout', views.logout_view, name='logout'),
+    path('login', views.login_view, name='login'),
+    path('activate/<slug:id>/<slug:token>/', views.activate, name='activate'),
+]
