448c0edb701efec5e196b62da9df63dbfcccfaa8
h5326 <jacek.tomczak-ext@man.eu>
2019-06-05 16:01:53 +0200

    dodano przekierowanie do starej wersji aplikacji, nie dziala logowanie

--- aplikacja_MAN/planowanie/views.py
+++ aplikacja_MAN/planowanie/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def redirect_to_old_version(request):
+    return redirect('http://10.131.80.125:5000/')
 
 # Create your views here.
