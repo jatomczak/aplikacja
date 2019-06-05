48ae00ae9a278a6935c17fe26f002cc7717aefd3
h5326 <jacek.tomczak-ext@man.eu>
2019-06-05 15:29:55 +0200

    dodano widok index.html

--- aplikacja_MAN/aplikacja_MAN/urls.py
+++ aplikacja_MAN/aplikacja_MAN/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index_view
 
 urlpatterns = [
+    path('', index_view),
     path('admin/', admin.site.urls),
     path('clients/', include('clients.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
