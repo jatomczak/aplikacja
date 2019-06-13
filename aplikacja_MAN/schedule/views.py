f5feda6c829c4d910f9b3818d36398062bb0ecab
h5326 <jacek.tomczak-ext@man.eu>
2019-06-13 16:08:11 +0200

    zwraca liste users_id z danego dzialu

--- aplikacja_MAN/schedule/views.py
+++ aplikacja_MAN/schedule/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from clients.models import User
 # Create your views here.
 
 @login_required
 def home_view(request):
-    department = request.user.group.group_name
-    return HttpResponse('Widok glowny dla dzialu %s '% department)
+    department = request.user.group
+    user_list = User.objects.filter(group=department)
+    user_id_list = []
+    for user in user_list:
+        user_id_list.append(user.user_id)
+    return HttpResponse(user_id_list)
