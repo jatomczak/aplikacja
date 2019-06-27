6f84bf56504120c1b2ffed4f2cb6f0b880007163
Puchalski <Pawel.Puchalski-ext@man.eu>
2019-06-27 08:55:38 +0200

    spolszczenie fraz w harmonogramie

--- aplikacja_MAN/schedule/models.py
+++ aplikacja_MAN/schedule/models.py
@@ -3,5 +3,5 @@ from datetime import date
 
 # Create your models here.
 class VacationTimeRangeModel(models.Model):
-    date_from = models.DateField(default=date.today())
-    date_to = models.DateField(default=date.today())
+    date_from = models.DateField("Data od: ", default=date.today())
+    date_to = models.DateField("Data do: ", default=date.today())
