7d03dcc283fb2aebd52676930d91258b9bced217
h5326 <jacek.tomczak-ext@man.eu>
2019-07-03 14:01:15 +0200

    doano kolejne testy SelectRimeRangeForm, test incorrect_date_format czerwony

--- aplikacja_MAN/schedule/tests/tests_forms.py
+++ aplikacja_MAN/schedule/tests/tests_forms.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from schedule import forms
 
@@ -19,3 +20,16 @@ class SelectTimeRangeFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('Bledny format daty', form.errors['date_from'])
         self.assertIn('Bledny format daty', form.errors['date_to'])
+
+    def test_correct_fill_form(self):
+        form = forms.SelectTimeRangeForm(data={'date_from': '2019-02-10', 'date_to': '2019-02-12'})
+        self.assertTrue(form.is_valid())
+        self.assertEqual({}, form.errors)
+
+    def test_defaults_values(self):
+        form = forms.SelectTimeRangeForm()
+        print(form)
+        date_from = form['date_from'].value()
+        date_to = form['date_to'].value()
+        self.assertEqual(date_from.date(), timezone.now().date())
+        self.assertEqual(date_to.date(), timezone.now().date())
