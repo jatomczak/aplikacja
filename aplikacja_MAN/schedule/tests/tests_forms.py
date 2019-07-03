ee309ebf935e6014e67ea0a174bfd412249133a2
h5326 <jacek.tomczak-ext@man.eu>
2019-07-03 12:04:06 +0200

    dodano dwa testy SelectTimeRangeForm

--- aplikacja_MAN/schedule/tests/tests_forms.py
+++ aplikacja_MAN/schedule/tests/tests_forms.py
@@ -3,7 +3,19 @@ from django.test import TestCase
 from schedule import forms
 
 
-class ScheduleFormTest(TestCase):
+class SelectTimeRangeFormTest(TestCase):
     def test_incorrect_date_range(self):
         form = forms.SelectTimeRangeForm(data={'date_from':'2019-02-10', 'date_to':'2019-02-01'})
         self.assertFalse(form.is_valid())
+        self.assertIn("Blędny zakres dat", form.errors['date_to'])
+
+    def test_too_long_date_range(self):
+        form = forms.SelectTimeRangeForm(data={'date_from':'2019-01-01', 'date_to':'2019-10-10'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Maksymalny przedział nie może być dłuższy niż 31 dni.', form.errors['date_to'])
+
+    def test_incorrect_date_format(self):
+        form = forms.SelectTimeRangeForm(data={'date_from': '10-10-2019', 'date_to': '10-10-2019'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Bledny format daty', form.errors['date_from'])
+        self.assertIn('Bledny format daty', form.errors['date_to'])
